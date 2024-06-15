6a5fd76bb1f82f62792eb48cbd3dc0963a2edaa4
shahin2525 <shahin451999@gmail.com>
2024-06-14 23:38:12 -0700

    first commit

---
+++ js/phone-hunter.js
@@ -0,0 +1,34 @@
+const loadPhoneHunter = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/phones?search=iphone"
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhone(phones);
+};
+
+const displayPhone = (phones) => {
+  const phoneContainer = document.getElementById("card-container");
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add = `card w-90 bg-gray-100 shadow-xl`;
+    phoneDiv.innerHTML = `
+     <figure class="px-10 pt-10">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+            alt="Shoes"
+            class="rounded-xl"
+          />
+        </figure>
+        <div class="card-body items-center text-center">
+          <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+   `;
+    phoneContainer.appendChild(phoneDiv);
+  });
+};
+loadPhoneHunter();
